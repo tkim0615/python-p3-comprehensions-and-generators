14c16af036275c8d710997cf9a381dbe9ee3c5a4
tyler kim <yoonsoo.kim11@gmail.com>
2024-01-07 10:24:55 -0500

    completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [n for n in num_list if n % 2 ==0]
+print(return_evens([2,3,4,5,6]))
 
 def make_exclamation(sentence_list):
-    pass
+    return [n + "!" for n in sentence_list]
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
+# Using a list comprehension, write a function make_exclamation() that takes a 
+# list of sentence strings and returns a list of sentence strings with exclamation
+# marks at the end.
+
+# make_exclamation(["Hello", "I'm doing great", "Python is fun"])
+# # ["Hello!", "I'm doing great!", "Python is fun!"]
